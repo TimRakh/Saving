fcce72af7bb570d40076babb714aee44d9af8647
Tim Rakh <nbveh0914@gmail.com>
2023-02-10 15:50:02 +0300

    commits

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        GameProgress pl1 = new GameProgress(67, 4, 9, 2.14);
+        GameProgress pl2 = new GameProgress(81, 9, 61, 9.23);
+        GameProgress pl3 = new GameProgress(99, 2, 2, 0.9);
+
+        saveGame("D://Games//savegames//save.dat", pl1);
+        saveGame("D://Games//savegames//save1.dat", pl2);
+        saveGame("D://Games//savegames//save2.dat", pl3);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("D://Games//savegames//save.dat");
+        list.add("D://Games//savegames//save1.dat");
+        list.add("D://Games//savegames//save2.dat");
+        zipFiles("D://Games//savegames//zip.zip",list);
+        File game1Dat = new File("D://Games//savegames//save.dat.dat");
+        File game2Dat = new File("D://Games//savegames//save1.dat");
+        File game3Dat = new File("D://Games//savegames//save2.dat");
+        if (game1Dat.delete()) System.out.println("Файл \"save.dat\" удален");
+        if (game2Dat.delete()) System.out.println("Файл \"save1.dat\" удален");
+        if (game3Dat.delete()) System.out.println("Файл \"save2.dat\" удален");
+    }
+
+    public static void saveGame(String adress, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(adress);
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(gameProgress);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void zipFiles(String adress, ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            String s = list.get(i);
+            try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream("zip_output.zip"));
+                 FileInputStream fis = new FileInputStream(s)) {
+                ZipEntry entry = new ZipEntry("packed_notes.txt");
+                zout.putNextEntry(entry);
+                byte[] buffer = new byte[fis.available()];
+                fis.read(buffer);
+                zout.write(buffer);
+                zout.closeEntry();
+
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
